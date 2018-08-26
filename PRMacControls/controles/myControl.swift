6e40d04eff2993bf4c07518ff33bf9f61dd2453f
Patrice Rapaport <patrice@rapaport.fr>
2018-08-26 08:16:39 +0200

    delayfocus

--- PRMacControls/controles/myControl.swift
+++ PRMacControls/controles/myControl.swift
@@ -575,14 +575,15 @@ open class cmyControl: NSObject {
     
     public func popover (_ msg: String) {
         let ident: String = identifier
-        Swift.print ("appel de popover pour \(ident)")
+        let pass = (ctrl as! cmyTextfield).passage
+        Swift.print ("\(pass) appel de popover pour \(ident)")
         if controller is cbaseController {
             (controller as! cbaseController).showPopover(aControl: ctrl as! NSControl, msg: msg)
         }
         else if controller is cbaseView {
             (controller as! cbaseView).showPopover(aControl: ctrl as! NSControl, msg: msg)
         }
-        Swift.print ("popover appelé  pour \(ident)")
+        Swift.print ("\(pass)  popover appelé  pour \(ident)")
     }
     
     public func popover (aController: NSViewController) {
